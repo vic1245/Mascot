426dc455d9251c5d018eeb5d84cfef66c2e2b6d5
Victor <vic12448@hotmail.com>
2021-02-28 16:38:15 -0500

    prueba

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1>Attention to Pets</h1>
+      <hr/>
+      <div LassName="row"> 
+      <div className="col-8">
+      <h4 className="text">PetName:
+        <form>
+          <input
+            type="text"
+            className="form"
+            placeholder=" "
+          />
+        </form>
+      </h4>
+      <h4 className="text">PetType:
+      <form>
+          <input
+            type="text"
+            className="form"
+            placeholder=" "
+          />
+        </form>
+      </h4>
+
+      <h4 className="text">PetBreed:
+      <form>
+          <input
+            type="text"
+            className="form"
+            placeholder=" "
+          />
+        </form>
+      </h4>
+
+      <h4 className="text">PetDate:
+      <form>
+          <input
+            type="text"
+            className="form"
+            placeholder=" "
+          />
+        </form>
+      </h4>
+
+      <h4 className="text">OwnerName:
+      <form>
+          <input
+            type="text"
+            className="form"
+            placeholder=" "
+          />
+        </form>
+      </h4>
+
+      <h4 className="text">OwnerPhone:
+      <form>
+          <input
+            type="text"
+            className="form"
+            placeholder=" "
+          />
+        </form>
+
+      </h4>
+      <h4 className="text">OwnerAddress:
+      <form>
+          <input
+            type="text"
+            className="form"
+            placeholder=" "
+          />
+        </form>
+      </h4>
+
+      <h4 className="text">OwnerEmail:
+      <form >
+          <input
+            type="text"
+            className="form"
+            placeholder=" "
+          />
+        </form>
+      </h4>
+          <button className="btn btn-success btn-block"
+          type="submit"
+          >Add</button>
+
+          <button className="btn btn-dark btn-block"
+          type="submit"
+          >Remove</button>
+          <h1>hola</h1>
+
+      </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
