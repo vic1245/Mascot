9f06cf7811e2eb9da2237986e9b8ac29571ca321
Victor <vic12448@hotmail.com>
2021-02-28 17:29:54 -0500

    agregar ventana modal y tabla

--- src/App.js
+++ src/App.js
@@ -1,104 +1,77 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Button, Modal, ModalHeader, ModalBody, Table } from "reactstrap";
 
 function App() {
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
   return (
     <div className="container mt-5">
-      <button>Add Pet</button>
+      <Button color="danger" onClick={toggle}>
+        Add Mascot
+      </Button>
+      <Modal isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle}>Add Mascot</ModalHeader>
+        <ModalBody>
+          <form>
+            <input type="text" className="form" placeholder=" Pet Name" />
+            <input type="text" className="form" placeholder="Pet Type" />
+            <input type="text" className="form" placeholder="Pet Breed" />
+            <input type="text" className="form" placeholder="Pet Date " />
+            <input type="text" className="form" placeholder="Owner Name " />
+            <input type="text" className="form" placeholder="Owner Phone " />
+            <input type="text" className="form" placeholder="Owner Address " />
+
+            <input type="text" className="form" placeholder="Owner Email " />
+
+            <button className="btn btn-success btn-block" type="submit">
+              Add
+            </button>
+
+            <button className="btn btn-dark btn-block" onClick={toggle}>
+              Close
+            </button>
+          </form>
+        </ModalBody>
+      </Modal>
       <h1>Attention to Pets</h1>
-      <hr/>
-      <div LassName="row"> 
-      <div className="col-8">
-      <h4 className="text">PetName:
-        <form>
-          <input
-            type="text"
-            className="form"
-            placeholder=" "
-          />
-        </form>
-      </h4>
-      <h4 className="text">PetType:
-      <form>
-          <input
-            type="text"
-            className="form"
-            placeholder=" "
-          />
-        </form>
-      </h4>
-
-      <h4 className="text">PetBreed:
-      <form>
-          <input
-            type="text"
-            className="form"
-            placeholder=" "
-          />
-        </form>
-      </h4>
-
-      <h4 className="text">PetDate:
-      <form>
-          <input
-            type="text"
-            className="form"
-            placeholder=" "
-          />
-        </form>
-      </h4>
-
-      <h4 className="text">OwnerName:
-      <form>
-          <input
-            type="text"
-            className="form"
-            placeholder=" "
-          />
-        </form>
-      </h4>
-
-      <h4 className="text">OwnerPhone:
-      <form>
-          <input
-            type="text"
-            className="form"
-            placeholder=" "
-          />
-        </form>
-
-      </h4>
-      <h4 className="text">OwnerAddress:
-      <form>
-          <input
-            type="text"
-            className="form"
-            placeholder=" "
-          />
-        </form>
-      </h4>
-
-      <h4 className="text">OwnerEmail:
-      <form >
-          <input
-            type="text"
-            className="form"
-            placeholder=" "
-          />
-        </form>
-      </h4>
-          <button className="btn btn-success btn-block"
-          type="submit"
-          >Add</button>
-
-          <button className="btn btn-dark btn-block"
-          type="submit"
-          >Remove</button>
-          
-
-      </div>
-      </div>
+      <hr />
+      <Table dark>
+        <thead>
+          <tr>
+            <th>Name pet</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
