57198c4c60424b7452ad55a847f579baa7cbc09c
Victor <vic12448@hotmail.com>
2021-02-28 22:55:21 -0500

    Se agregan variables y funciones

--- src/App.js
+++ src/App.js
@@ -1,19 +1,119 @@
+
 import React, { useState } from "react";
 import { Button, Modal, ModalHeader, ModalBody, Table } from "reactstrap";
+import { isEmpty } from "lodash";
+import { addDocument, deleteDocument, getCollection, updateDocument } from './actions'
 
 function App() {
   const [modal, setModal] = useState(false);
+  const [mascots, setMascot] = useState("")
+  const [mascots, setMascots] = useState([])
+  const [editMode, setEditMode] = useState(false)
+  const [id, setId] = useState("")
+  const [error, setError] = useState(null)
 
+  useEffect(() => {
+    (async () => {
+      const result = await getCollection("mascots")
+      if (result.statusResponse) {
+        setTasks(result.data)
+      }
+    })()
+  }, [])
+
+  const validForm = () => {
+    let isValid = true
+    setError(null)
+
+    if (isEmpty(mascots)) {
+      setError("Debes ingresar una mascota")
+      isValid = false
+    }
+    
+    return isValid
+  }
+
+  const addMascot= async(e) => {
+    e.preventDefault()
+
+    if (!validForm()) {
+      return
+    }
+
+    const result = await addDocument("mascots", { name: mascot })
+    if (!result.statusResponse) {
+      setError(result.error)
+      return
+    }
+
+    setMascots([ ...mascots, { id: result.data.id, name: mascot } ])
+    setMascot("")
+  }
+
+  const saveMascot = async(e) => {
+    e.preventDefault()
+
+    if (!validForm()) {
+      return
+    }
+
+    const result = await updateDocument("mascots", id, { name: mascot })
+    if (!result.statusResponse) {
+      setError(result.error)
+      return
+    } 
+
+    const editedMascot = mascot.map(item => item.id === id ? { id, name: mascot} : item)
+    setTasks(editedMascots)
+    setEditMode(false)
+    setMascot("")
+    setId("")
+  }
+
+    const result = await updateDocument("mascots", id, { name: mascot })
+    if (!result.statusResponse) {
+      setError(result.error)
+      return
+    } 
+
+    const editedMascot = mascot.map(item => item.id === id ? { id, name: mascot} : item)
+    setTasks(editedMascots)
+    setEditMode(false)
+    setMascot("")
+    setId("")
+  }
+
+  const deleteMascot = async(id) => {
+    const result = await deleteDocument("mascots", id)
+    if (!result.statusResponse) {
+      setError(result.error)
+      return
+    }
+
+    const filteredMascots = mascots.filter(mascot => mascot.id !== id)
+    setMascots(filteredMascots)
+  }
+
+  const editTask = (theTask) => {
+    setTask(theTask.name)
+    setEditMode(true)
+    setId(theTask.id)
+  }
+
+  
   const toggle = () => setModal(!modal);
   return (
     <div className="container mt-5">
       <Button color="danger" onClick={toggle}>
         Add Mascot
+        onChange={(text) => setModal(test.target.value)}
+        mascot = () => ModalBody
+        value={mascot}
       </Button>
       <Modal isOpen={modal} toggle={toggle}>
         <ModalHeader toggle={toggle}>Add Mascot</ModalHeader>
         <ModalBody>
-          <form>
+          <form onSubmit={() => mascot }>
             <input type="text" className="form" placeholder=" Pet Name" />
             <input type="text" className="form" placeholder="Pet Type" />
             <input type="text" className="form" placeholder="Pet Breed" />
@@ -21,7 +121,6 @@ function App() {
             <input type="text" className="form" placeholder="Owner Name " />
             <input type="text" className="form" placeholder="Owner Phone " />
             <input type="text" className="form" placeholder="Owner Address " />
-
             <input type="text" className="form" placeholder="Owner Email " />
 
             <button className="btn btn-success btn-block" type="submit">
@@ -60,7 +159,10 @@ function App() {
             <td>@mdo</td>
             <td>@mdo</td>
             <td>@mdo</td>
-            <td>Mark</td>
+            <td>Mark
+            <button className="btn btn-danger btn-sm float-right mx-2">Remove</button>
+              <button className="btn  btn-warning btn-sm float-right">Edit</button>
+            </td>
           </tr>
           <tr>
             <th scope="row">2</th>
